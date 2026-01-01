3133fef6be35cd10a8eba6e6196985b3374366f0
Rumail Abbas <rumailabbas@192.168.68.105>
2026-01-01 23:39:35 +0700

    Fix base path untuk Vercel

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/bumdes/', // Nama repository di GitHub
+  base: '/', // Nama repository di GitHub
 })
